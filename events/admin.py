0d4b794df951829f03f82f6c4ecec244506ff9ec
Damián Silvani <munshkr@gmail.com>
2020-12-06 14:35:12 -0300

    Create Participant and EventSlot models

--- events/admin.py
+++ events/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
 
-from events.models import Event
+from events.models import Event, EventSlot, Participant
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'url')
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'starts_at', 'ends_at', 'active')
 
 
+class EventSlotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_event_name', 'starts_at', 'ends_at',
+                    'get_participant_name')
+
+    def get_event_name(self, obj):
+        return obj.event.name
+
+    get_event_name.short_description = 'Event'
+    get_event_name.admin_order_field = 'event__name'
+
+    def get_participant_name(self, obj):
+        return obj.participant.name
+
+    get_participant_name.short_description = 'Participant'
+    get_participant_name.admin_order_field = 'participant__name'
+
+    def get_date_range(self, obj):
+        return f'{obj.starts_at} - {obj.ends_at}'
+
+    get_date_range.short_description = 'Date range'
+    get_date_range.admin_order_field = ('starts_at', 'ends_at')
+
+
+admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(EventSlot, EventSlotAdmin)
--- events/models.py
+++ events/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Participant(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'{self.name} <{self.email}>'
+
+
 class Event(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
@@ -8,3 +17,18 @@ class Event(models.Model):
     starts_at = models.DateTimeField()
     ends_at = models.DateTimeField()
     active = models.BooleanField(default=True)
+    participants = models.ManyToManyField(Participant, through='EventSlot')
+
+    def __str__(self):
+        return f'{self.name} ({self.starts_at} - {self.ends_at})'
+
+
+class EventSlot(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    stream_key = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.event.name}: {self.participant} ({self.starts_at} - {self.ends_at})'
