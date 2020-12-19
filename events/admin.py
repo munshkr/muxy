7f4d9dbc0c1fd920d46ec6029840ae22e0749f31
Damián Silvani <munshkr@gmail.com>
2020-12-19 18:34:27 +0000

    Add filter and orering on Stream admin

--- events/admin.py
+++ events/admin.py
@@ -6,6 +6,7 @@ from events.models import Event, Stream, StreamNotification
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'starts_at', 'ends_at', 'active')
+    ordering = ('-starts_at',)
 
 
 class StreamAdmin(admin.ModelAdmin):
@@ -14,6 +15,8 @@ class StreamAdmin(admin.ModelAdmin):
     list_display_links = ('slug', )
     exclude = ('live_at', )
     form = StreamForm
+    ordering = ('starts_at',)
+    list_filter = ('event',)
 
     def get_event_name(self, obj):
         return obj.event.name
