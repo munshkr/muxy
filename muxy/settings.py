9eef5ae8200792e68819b83df12e0834e448c8a7
Damián Silvani <munshkr@gmail.com>
2021-02-17 18:13:27 -0300

    Add a default timeout of 20s for the sqlite3 connection

--- muxy/settings.py
+++ muxy/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20
+        }
     }
 }
 
