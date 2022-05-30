f9c9ff82d22d08cf10d9ded898f8d784b6c8ec5c
Damián Silvani <munshkr@gmail.com>
2022-05-30 18:02:53 -0300

    Add tests

--- events/tests.py
+++ events/tests.py
@@ -196,6 +196,32 @@ class StreamTests(MuxyAPITestCase):
         self.assertTrue("key" not in response.data)
         self.assertTrue("publisher_email" not in response.data)
 
+    def test_update_stream_set_archive_urls(self):
+        """
+        Ensure we can update archive URLs for Stream.
+
+        """
+        event = self.create_some_event()
+        stream = self.create_some_stream(event)
+        self.authenticate_with_api_key(is_web=False)
+
+        data = {
+            "archive_urls": [
+                { "url": "https://archive.org/foo", "name": "archive.org" },
+                { "url": "https://youtube.com/foo", "name": "youtube.com" },
+            ]
+        }
+        response = self.client.patch(
+            reverse("stream-detail", kwargs={"pk": stream.pk}), data, format="json"
+        )
+
+        # Assert response
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(response.data.get("archive_urls"), data["archive_urls"])
+
+        # Assert database
+        self.assertEqual(stream.archive_urls.count(), 2)
+
     def create_some_event(self):
         starts_at = timezone.make_aware(datetime.today())
         ends_at = starts_at + timedelta(hours=2)
